0f9afa54aae466310b6089f8b164b5b7ec1c6a85
turian79 <theo.t.robert@gmail.com>
2025-10-08 11:56:36 +0200

    Hello World fonctionnel sur une fenetre

--- src/Demineur/demineur.java
+++ src/Demineur/demineur.java
@@ -1,11 +1,17 @@
 package Demineur;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class demineur extends JFrame
 {
     public demineur ()
     {
         super("DemineurV1");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        add(new JLabel("Hello World!", SwingConstants.CENTER), BorderLayout.CENTER);
     }
 }
